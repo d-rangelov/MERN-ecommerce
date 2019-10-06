e1d4b7b1b4cfc1d9592d861d578ffe3dcb2c9462
Denis <cru3lgenius@gmail.com>
2019-10-06 15:02:41 +0200

    remove gitignore files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-function Home() {
+import React, { useEffect } from "react";
+import axios from "axios";
+
+function Home({ products }) {
+  console.log(products);
+
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  // fetch data on server
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+
+  return { products: response.data };
+  // this  object will be merged with existing props
+};
 
 export default Home;
