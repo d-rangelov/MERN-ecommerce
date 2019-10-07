59a1068bcdc56c7be44de04038b7ca8c5f73537d
Denis <cru3lgenius@gmail.com>
2019-10-07 23:47:47 +0200

    Add Semantic UI cards to represent the data from MongoDB

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import axios from "axios";
+import ProductList from "../components/Index/ProductList";
 
 function Home({ products }) {
-  console.log(products);
-
-  return <>home</>;
+  return <ProductList products={products} />;
 }
 
 Home.getInitialProps = async () => {
-  // fetch data on server
   const url = "http://localhost:3000/api/products";
   const response = await axios.get(url);
-
   return { products: response.data };
-  // this  object will be merged with existing props
 };
 
 export default Home;
